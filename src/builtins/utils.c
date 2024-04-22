cf47e9d753e70accf27fe946f4090106bbc353d9
alexthill <thial058@gmail.com>
2024-04-22 11:29:06 +0200

    fixed echo

--- src/builtins/utils.c
+++ src/builtins/utils.c
@@ -6,7 +6,7 @@
 /*   By: elo <elo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 13:32:11 by athill            #+#    #+#             */
-/*   Updated: 2024/04/19 19:04:31 by elo              ###   ########.fr       */
+/*   Updated: 2024/04/22 10:41:03 by athill           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ int	exec_builtin(t_data *data, char **args)
 {
 	if (ft_streq(args[0], "exit"))
 		return (cmd_exit(data, args));
-	else if(ft_streq(args[0],"echo"))
+	else if (ft_streq(args[0], "echo"))
 		return (cmd_echo(args));
-	else if(ft_streq(args[0],"cd"))
+	else if (ft_streq(args[0], "cd"))
 		return (cmd_cd(data,args));
 	return (0);
 }
 
 int	is_builtin(char const *name)
 {
-	return (ft_streq(name, "exit"));
+	return (ft_streq(name, "exit")
+		|| ft_streq(name, "cd")
+		|| ft_streq(name, "echo"));
 }
