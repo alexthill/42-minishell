e8669fa8e756b2f738ab9962063446964f0bafd3
Eyzou <elodie.hamm@gmail.com>
2024-04-23 11:19:23 +0200

    modified.h

--- include/builtins.h
+++ include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: ehamm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 11:24:31 by athill            #+#    #+#             */
-/*   Updated: 2024/04/23 11:07:03 by ehamm            ###   ########.fr       */
+/*   Updated: 2024/04/23 11:18:54 by ehamm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,5 @@ int		is_builtin(char const *name);
 int		cmd_exit(t_data *data, char **args);
 int		cmd_echo(t_data *data, char **args);
 int		cmd_cd(t_data *data, char **args);
-
-
-//cd.c
-char	*get_env_var(t_data *data, char *name);
-int		set_env_var(t_data *data, char *name,char *value);
-char	**ft_split2(const char *s, char c);
 
 #endif
