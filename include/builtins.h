20fe671bc9fb4d9f3514861b03f64d4dfd1af1fa
Eyzou <elodie.hamm@gmail.com>
2024-04-23 11:09:27 +0200

    cd modified

--- include/builtins.h
+++ include/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elo <elo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: ehamm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 11:24:31 by athill            #+#    #+#             */
-/*   Updated: 2024/04/19 16:50:40 by elo              ###   ########.fr       */
+/*   Updated: 2024/04/23 11:07:03 by ehamm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,11 @@ int		is_builtin(char const *name);
 int		cmd_exit(t_data *data, char **args);
 int		cmd_echo(t_data *data, char **args);
 int		cmd_cd(t_data *data, char **args);
+
+
+//cd.c
+char	*get_env_var(t_data *data, char *name);
+int		set_env_var(t_data *data, char *name,char *value);
+char	**ft_split2(const char *s, char c);
 
 #endif
--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ehamm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:09:43 by elo               #+#    #+#             */
-/*   Updated: 2024/04/22 17:11:12 by ehamm            ###   ########.fr       */
+/*   Updated: 2024/04/23 11:08:10 by ehamm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,45 +16,70 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-char	*get_env_data(char *var)
+char	*get_env_var(t_data *data, char *name)
 {
-	t_env		*tmp;
-	size_t		len;
+	t_env	*node;
 
-	tmp = NULL;
-	len = ft_strlen(var);
-	while (tmp)
+	node = data->env;
+	while (node)
 	{
-		if (!(ft_strncmp(var, tmp->name, len)))
-			return (tmp->value);
-		tmp = tmp->next;
+		if (ft_streq(node->name, name))
+			return (node->value);
+		node = node->next;
 	}
 	return (NULL);
 }
-// WIP
-int	cmd_cd(char **args)
+
+int	set_env_var(t_data *data, char *name, char *value)
 {
-	char *pwd;
+	t_env	*node;
+
+	if (name == NULL || data == NULL || value == NULL)
+		return (1);
+	node = data->env;
+	while (node)
+	{
+		if (ft_streq(node->name, name))
+		{
+			free(node->value);
+			node->value = ft_strdup(value);
+			return (0);
+		}
+		node = node->next;
+	}
+	return (1);
+}
+
+int	cmd_cd(char **args, t_data *data)
+{
 	char *oldpwd;
 	char *path;
-	pwd = get_env_data("PWD");
-	oldpwd = get_env_data("OLDPWD");
 
-	if (args[1] == 0)
-		path = get_env_data("HOME");
-	if (path == 0)
+	oldpwd = get_env_var(data, "PWD");
+	if (oldpwd == NULL)
 	{
 		print_errno(0, "MSG_CD_ERR");
 		return (1);
 	}
+	if (args[1] == 0)
+		path = get_env_var(data, "HOME");
 	else
 		path = args[1];
+
 	if (chdir(path) == -1)
 	{
 		print_errno(0, "MSG_CD_ERR");
 		return (1);
 	}
-	else
-		ft_strlcpy(oldpwd, pwd, ft_strlen(pwd));
+	if (set_env_var(data, "OLDPWD", oldpwd) == 1)
+	{
+		print_errno(0, "MSG_CD_ERR");
+		return (1);
+	}
+	if (set_env_var(data, "PWD", path) == 1)
+	{
+		print_errno(0, "MSG_CD_ERR");
+		return (1);
+	}
 	return (0);
 }
